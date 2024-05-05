a5d77779d6daa2e192523aef54198f8fcbf4b50d
anzilabdulhakkim <anzilabdulhakkim135@gmail.com>
2024-05-06 02:02:09 +0530

    added the schemas and models

---
+++ Models/Order.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  products: [
+    {
+      productId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+        required: true,
+      },
+      quantity: { type: Number, required: true },
+    },
+  ],
+  totalAmount: { type: Number, required: true },
+  shippingAddress: { type: String },
+});
+
+const OrderModel = mongoose.model("Order", orderSchema);
+
+module.exports = OrderModel;
