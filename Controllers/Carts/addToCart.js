63fd5d021e1740a217af7950507b4a30bae2fd73
anzilabdulhakkim <anzilabdulhakkim135@gmail.com>
2024-05-06 01:58:54 +0530

    added the cart controller

---
+++ Controllers/Carts/addToCart.js
@@ -0,0 +1,24 @@
+const CartModel = require("../../Models/Cart");
+
+const addToCart = async (req, res) => {
+  const { userId, quantity = 1 } = req.body;
+  const { productId } = req.params;
+
+  try {
+    const existingCartItem = await CartModel.findOne({ userId, productId });
+
+    if (existingCartItem) {
+      existingCartItem.quantity += quantity;
+      return await existingCartItem.save();
+    }
+
+    const newCartItem = new CartModel({ userId, productId, quantity });
+    await newCartItem.save();
+    res.status(200).json({ message: "Item added to cart", newCartItem });
+  } 
+  catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+module.exports = addToCart;
---
+++ Controllers/Carts/getCartItems.js
@@ -0,0 +1,15 @@
+const CartModel = require("../../Models/Cart");
+
+const getCartItems = async (req, res) => {
+  const { userId } = req.body;
+
+  try {
+    const items = await CartModel.find({ userId });
+    res.json({ items });
+  } 
+  catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+module.exports = getCartItems;
